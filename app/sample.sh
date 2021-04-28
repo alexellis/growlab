d8b676d10f9a0a145826d2962de65625b23e8b2f
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2021-04-28 12:28:50 +0100

    Rename sample
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- app/sample.sh
+++ app/sample.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+rm html/*
+
 python3 app.py
 cp html/* ../docs/
+
 git add ..
 git commit -s -m "Update images at `date`"
 GIT_SSH_COMMAND="ssh -i `pwd`/.ssh/id_rsa" git pull origin master --rebase
