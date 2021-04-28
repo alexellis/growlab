2c8d96be835158d9bde4e6612400230333dd78ad
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2021-04-28 12:25:47 +0100

    Update template and generation
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- app/specimen.py
+++ app/specimen.py
@@ -57,6 +57,7 @@ class specimen:
         vals["temperature"] = "{:05.2f}{}C".format(readings["temperature"], degree_symbol)
         vals["humidity"] = "{:05.2f}%".format(readings["humidity"])
         vals["pressure"] = "{:05.2f}hPa".format(readings["pressure"])
+        vals["uid"] = "{}".format(time.time())
         html = template.render(vals)
 
         with open(output_path+"/index.html", "w") as html_file:
