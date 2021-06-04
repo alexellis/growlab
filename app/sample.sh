b7f3d1e6e0da9092e7376dfc8ca863b938f2985b
AMouchere <a.mouchere@protonmail.com>
2021-06-04 22:18:43 +0100

    Update images at Fri  4 Jun 22:18:43 BST 2021
    Signed-off-by: AMouchere <a.mouchere@protonmail.com>

--- app/sample.sh
+++ app/sample.sh
@@ -6,8 +6,10 @@ export GIT_SSH_COMMAND=""
 cp ${HOME}/growlab/app/html/* ${HOME}/growlab/docs/
 cd ${HOME}/growlab
 git add .
-
+echo "commit ..."
 git commit -s -m "Update images at `date`"
+echo "pull ..."
 git pull origin master --rebase
+echo "push ..."
 git push origin master
 
