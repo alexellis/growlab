3462626a46d70e9865af9ccf9dc32ff70014de83
AMouchere <a.mouchere@protonmail.com>
2021-06-04 21:58:54 +0100

    Update images at Fri  4 Jun 21:58:54 BST 2021
    Signed-off-by: AMouchere <a.mouchere@protonmail.com>

--- app/sample.sh
+++ app/sample.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 python3 app.py
 
-export GIT_SSH_COMMAND="ssh -i `pwd`/.ssh/id_rsa"
+export GIT_SSH_COMMAND=""
 
 cp ${HOME}/growlab/app/html/* ${HOME}/growlab/docs/
 cd ${HOME}/growlab
